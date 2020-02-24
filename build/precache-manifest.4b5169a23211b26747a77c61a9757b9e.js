98c329f32099f5d1a624ef391f527f3d446e7c79
Bartek <gekarez@gmail.com>
2020-02-24 17:17:01 +0100

    Add build application files

---
+++ build/precache-manifest.4b5169a23211b26747a77c61a9757b9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad446da4ff870fa4510228769b3b81c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1859eee233d2bf8f0217",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "a0bc4a19ff32cf370da1",
+    "url": "/static/css/main.36c4737a.chunk.css"
+  },
+  {
+    "revision": "1859eee233d2bf8f0217",
+    "url": "/static/js/2.8544ad2c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8544ad2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0bc4a19ff32cf370da1",
+    "url": "/static/js/main.6071ec80.chunk.js"
+  },
+  {
+    "revision": "304250442bfb27ff696b",
+    "url": "/static/js/runtime-main.c019587f.js"
+  }
+]);
