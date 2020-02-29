b2d860be9e2a583b76106a4cc1f1d84b655c23f0
bartek <gekarez@gmail.com>
2020-02-29 21:03:49 +0100

    Update files for deploy

---
+++ build/precache-manifest.dd043a10e7fda89bb530b0736d02dfa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d10b8a51ee9a807bb3acec6daa9b9e",
+    "url": "/looping-tasks_v2/index.html"
+  },
+  {
+    "revision": "1859eee233d2bf8f0217",
+    "url": "/looping-tasks_v2/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "f919db9d5f76f1403464",
+    "url": "/looping-tasks_v2/static/css/main.36c4737a.chunk.css"
+  },
+  {
+    "revision": "1859eee233d2bf8f0217",
+    "url": "/looping-tasks_v2/static/js/2.8544ad2c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/looping-tasks_v2/static/js/2.8544ad2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f919db9d5f76f1403464",
+    "url": "/looping-tasks_v2/static/js/main.3dcfcdfc.chunk.js"
+  },
+  {
+    "revision": "27c6e8788db85cf37ec1",
+    "url": "/looping-tasks_v2/static/js/runtime-main.499918c8.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import Tasks from './components/Tasks'
 function App() {
     return (
         <div className="App">
-            <Tasks />
+            <button className="button-success pure-button">Success Button</button>
+            <button className="button-error pure-button">Error Button</button>
+            <button className="button-warning pure-button">Warning Button</button>
+            <button className="button-secondary pure-button">Secondary Button</button>
+
+            <Tasks/>
         </div>
     );
 }
