1bc6c69654df72a2d13f5afaddb3df879bde8eda
bartek <gekarez@gmail.com>
2020-02-29 21:08:41 +0100

    clean

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import Tasks from './components/Tasks'
 function App() {
     return (
         <div className="App">
-            <button className="button-success pure-button">Success Button</button>
-            <button className="button-error pure-button">Error Button</button>
-            <button className="button-warning pure-button">Warning Button</button>
-            <button className="button-secondary pure-button">Secondary Button</button>
-
             <Tasks/>
         </div>
     );
