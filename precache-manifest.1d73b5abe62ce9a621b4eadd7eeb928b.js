c7dbab3f68a3923ff8d1f4a63d8bd23c706af85a
bartek <gekarez@gmail.com>
2020-02-29 21:16:57 +0100

    Updates

---
+++ precache-manifest.1d73b5abe62ce9a621b4eadd7eeb928b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c0cbf5dc04b30718d1b6a23225fb48",
+    "url": "/looping-tasks_v2/index.html"
+  },
+  {
+    "revision": "1859eee233d2bf8f0217",
+    "url": "/looping-tasks_v2/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "7167d8677e1795f058bd",
+    "url": "/looping-tasks_v2/static/css/main.36c4737a.chunk.css"
+  },
+  {
+    "revision": "1859eee233d2bf8f0217",
+    "url": "/looping-tasks_v2/static/js/2.8544ad2c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/looping-tasks_v2/static/js/2.8544ad2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7167d8677e1795f058bd",
+    "url": "/looping-tasks_v2/static/js/main.0ca5a5dc.chunk.js"
+  },
+  {
+    "revision": "27c6e8788db85cf37ec1",
+    "url": "/looping-tasks_v2/static/js/runtime-main.499918c8.js"
+  }
+]);
