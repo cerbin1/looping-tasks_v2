49dd4e8921dada05d0dc0c29efaceec1d6e43e4d
cerbin1 <gekarez@gmail.com>
2020-06-05 23:56:39 +0200

    Implement button and space pressing to automatically click next element

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,15 +8,22 @@ class Task extends Component {
         this.state = {
             name: props.name,
             elements: props.elements,
-            links: props.links
+            links: props.links,
+            index: 0
         };
+    }
+
+    componentDidMount() {
+        document.addEventListener("keydown", this.handleSpaceBarClick, false);
     }
 
     setElementClicked = (index) => {
         let elements = this.state.elements;
         elements[index].isClicked = !elements[index].isClicked;
+        let autoClickedElementIndex = this.state.index;
         this.setState({
-            elements: elements
+            elements: elements,
+            index: ++autoClickedElementIndex,
         })
     };
 
@@ -24,10 +31,33 @@ class Task extends Component {
         return this.state.elements.every(value => value.isClicked)
     };
 
+    autoClickNextElement = () => {
+        if (!this.allElementsClicked()) {
+            let elements = this.state.elements;
+            let autoClickedElementIndex = this.state.index;
+            elements[autoClickedElementIndex].isClicked = true;
+            this.setState({
+                elements: elements,
+                index: ++autoClickedElementIndex,
+            })
+        }
+    };
+
+    handleSpaceBarClick = (event) => {
+        if (event.keyCode === 32) {
+            this.autoClickNextElement();
+        }
+    };
+
     render() {
         return (
             <div>
                 <hr/>
+                <div className="row">
+                    <div className="col text-center mb-1">
+                        <button className={"btn btn-success btn-lg"} onClick={this.autoClickNextElement}>Done</button>
+                    </div>
+                </div>
                 {this.state.links == null ? null :
                     <div className="row">
                         <div className="col-xl-10 offset-1 text-center alert alert-primary">
