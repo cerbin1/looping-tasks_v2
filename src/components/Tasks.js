13b7e6c26ee7ee3044d7bbd2ddd87ffecc78cc7f
cerbin1 <gekarez@gmail.com>
2020-07-02 22:54:20 +0200

    Update task info

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -72,9 +72,10 @@ class Tasks extends Component {
                             name: "Przygotować docsa na następny miesiąc (Skopiować całe 3 kolumny, zmienić nazwę miesiąca)",
                             isClicked: false
                         },
-                        {name: "Opłacić media, abonamenty i kredyty i pobrać faktury", isClicked: false},
-                        {name: "Opłacić X", isClicked: false},
                         {name: "Zapisać aktualną kwotę w następnym miesiącu", isClicked: false},
+                        {name: "Sprawdzić czy zgadza się kwota pracy za ostatni miesiąc z przelewem", isClicked: false},
+                        {name: "Opłacić media, abonamenty i kredyty, pobrać faktury, zapisać kwoty", isClicked: false},
+                        {name: "Opłacić X", isClicked: false},
                         {name: "Zapisać pracę biorąc pod uwagę godziny", isClicked: false},
                         {name: "Uzupełnić kwoty z apki", isClicked: false},
                         {name: "Uzupełnić resztę kwot w arkuszu", isClicked: false},
