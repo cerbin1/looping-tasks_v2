67765f6750e680d1434a6d5677c0025d4089b0ff
cerbin <gekarez@gmail.com>
2021-01-16 14:19:02 +0100

    Extract data to Data.js to separate it from app logic

---
+++ src/Data.js
@@ -0,0 +1,172 @@
+class Data {
+    static taskData = [
+        {
+            name: "Porządki",
+            links: [
+                {
+                    name: "Quick Notes",
+                    link: "https://docs.google.com/document/d/1lKqrQYRtmsOP9UGHxnAQOOw01lfaMSjDvesVK4l-8wo/edit#heading=h.enryj93vpczc"
+                },
+                {
+                    name: "Google Drive",
+                    link: "https://drive.google.com/drive/my-drive"
+                },
+            ],
+            elements: [
+                "Organizacja maila głównego",
+                "Organizacja maila prywatnego",
+                "Utworzyć taski z Make Notes",
+                "Quick Notes",
+                "Notatki w telefonie",
+                "Google Drive",
+            ]
+        },
+        {
+            name: "Backup Soft",
+            links: [
+                {
+                    name: "Google backup",
+                    link: "https://takeout.google.com/settings/takeout"
+                },
+            ],
+            elements: [
+                "[Google] Calendar",
+                "[Google] Chrome (Bookmarks)",
+                "[Google] Drive (Notes)",
+                "Skopiować folder Documents -> _Backup_Soft_",
+            ]
+        },
+        {
+            name: "Backup Hard",
+            links: [
+                {
+                    name: "Google backup",
+                    link: "https://takeout.google.com/settings/takeout"
+                },
+            ],
+            elements: [
+                "OneNote cały notatnik jako doc",
+                "Folder Documents (Ubuntu)",
+                "Folder Dane (Dysk D)",
+                "Chrome Passwords Export",
+                "[Google] Calendar",
+                "[Google] Chrome (all)",
+                "[Google] Contacts",
+                "[Google] Drive",
+                "[Google] Mail",
+            ]
+        },
+        {
+            name: "Nowy miesiąc: Biznes",
+            links: [
+                {
+                    name: "Gaz",
+                    link: "https://ebok.pgnig.pl/"
+                },
+                {
+                    name: "Prąd",
+                    link: "https://logowanie.tauron.pl/"
+                },
+                {
+                    name: "PKO",
+                    link: "https://www.ipko.pl//"
+                },
+                {
+                    name: "mBank",
+                    link: "https://online.mbank.pl/pl/Login"
+                },
+                {
+                    name: "Alior",
+                    link: "https://system.aliorbank.pl/sign-in"
+                },
+                {
+                    name: "Sheet",
+                    link: "https://docs.google.com/spreadsheets/d/1RS4WCPEqILRgId3JlfXZuRd_f6kWZtYg1hzfhM8Y4n8/edit#gid=963549029"
+                },
+            ],
+            elements: [
+                "Przywrócić czarny kolor kolejnej kolumnie",
+                "Sprawdzić czy zgadza się kwota pracy za ostatni miesiąc z przelewem",
+                "Opłacić media, abonamenty i kredyty, pobrać faktury i potwierdzenia płatności, zapisać kwoty",
+                "Opłacić X",
+                "Zapisać pracę biorąc pod uwagę godziny",
+                "Uzupełnić kwoty z apki",
+                "Uzupełnić resztę kwot w arkuszu", "Zaktualizować kwoty w arkuszu Oszczędności po opłaceniu wszystkiego na ten miesiąc",
+                "Zapisać zsumowaną kwotę na start kolejnego miesiąca",
+                "Przeanalizować ostatni miesiąc",
+            ]
+        },
+        {
+            name: "Nowy miesiąc: TODO",
+            links: [
+                {
+                    name: "Sheets",
+                    link: "https://docs.google.com/spreadsheets/d/1RS4WCPEqILRgId3JlfXZuRd_f6kWZtYg1hzfhM8Y4n8/edit#gid=82903271/"
+                },
+            ],
+            elements: [
+                "Skopiować dwie pierwsze kolumny",
+                "Zmienić nazwę miesiąca",
+                "Zmienić kolor drugiej kolumny na czarny",
+                "Insert daty i ustawienie na 1 dzień",
+                "Pogrubić dzień i zastosować skrót ",
+                "Zaznaczyć pierwszy dzien -> Format -> Text rotation -> Rotate Up",
+                "Przeciągnąć dni do końca miesiąca",
+                "Wypełnić wzorcem wszystkie dni",
+                "Skopiować nie zaznaczony checkbox z któregoś pola",
+                "Zaznaczyć z ctrl wszystkie puste pola", "Wkleić wcześniej zaznaczonego checkboxa (nie wstawić, trzeba skopiować z innego pola checkbox i dopiero skopiować)",
+                "Uruchomić automatyzację do pythona (alias a1)",
+                "Wstawić formatowanie dat i checkboxów",
+                "Ukryć wiersze z czynności za poprzedni miesiąc",
+                "Ustawić kolor kolumny ze wzorem na biały", "Instrukcje z końca co do formatowania przekopiować niżej żeby nie przeszkadzały",
+            ]
+        },
+        {
+            name: "Nowy miesiąc: Trening",
+            links: [
+                {
+                    name: "Sheets",
+                    link: "https://docs.google.com/spreadsheets/d/1RS4WCPEqILRgId3JlfXZuRd_f6kWZtYg1hzfhM8Y4n8/edit#gid=568847193/"
+                },
+            ],
+            elements: [
+                "Wpisać nowy miesiąc i pogrubić",
+                "Insert daty i ustawienie na 1 dzień miesiąca",
+                "Sformatować datę używając pędzla i daty poprzedniej",
+                "Wstawić obok pusty checkbox",
+                "Przeciągnąć datę i checkbox do końca miesiąca",
+                "Uruchomić automatyzację do pythona (alias a2)",
+                "Wstawić formułę do dni i przeciągnąć do końca miesiąca",
+                "Wstawić formuły zliczające",
+                "Skopiować szkic formuł do następnego miesiąca na dół",
+                "Szybka analiza miesiąca",
+                "Ukryć rekordy z poprzedniego miesiąca",
+            ]
+        },
+        {
+            name: "Comiesięczne czynności",
+            links: [
+                {
+                    name: "Docs",
+                    link: "https://docs.google.com/document/d/1lKqrQYRtmsOP9UGHxnAQOOw01lfaMSjDvesVK4l-8wo/edit#heading=h.b36bz1pk3gth"
+                },
+                {
+                    name: "Sheets Szekle",
+                    link: "https://docs.google.com/spreadsheets/d/1u4sHO-mkux9d0xELt6XRo3IVpdoQrDb0cvN-U4ZQZ-E/edit#gid=0"
+                },
+            ],
+            elements: [
+                "Przebadać się (p i j)",
+                "Przetestować czujki",
+                "Sprawdzić luzy i stan śrub w hulajnodze, dopompować dętki",
+                "Zaaplikować sól do zmywarki",
+                "Kupić potrzebne rzeczy z docsa",
+                "Organizacja Temp Docs",
+                "Aktualizacja arkusza z cyklicznymi płatnościami",
+                "Zaktualizować listę znajomych (todoist i hierarchia onenote)",
+            ]
+        },
+    ]
+}
+
+export default Data;
