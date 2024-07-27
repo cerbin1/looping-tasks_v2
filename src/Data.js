144b98be560bee4eb07ce205f61fb6796e3d0a4a
cerbin1 <gekarez@gmail.com>
2024-07-27 23:26:31 +0200

    Update data

--- src/Data.js
+++ src/Data.js
@@ -1,182 +1,187 @@
 class Data {
-    static taskData = [
+  static taskData = [
+    {
+      name: "Porządki",
+      links: [
         {
-            name: "Porządki",
-            links: [
-                {
-                    name: "Quick Notes",
-                    link: "https://docs.google.com/document/d/1lKqrQYRtmsOP9UGHxnAQOOw01lfaMSjDvesVK4l-8wo/edit#heading=h.enryj93vpczc"
-                },
-                {
-                    name: "Google Drive",
-                    link: "https://drive.google.com/drive/my-drive"
-                },
-            ],
-            elements: [
-                "Organizacja maila głównego",
-                "Organizacja maila prywatnego",
-                "Utworzyć taski z Make Notes",
-                "Quick Notes",
-                "Organizacja notatek w keeps",
-                "Google Drive",
-            ]
+          name: "Quick Notes",
+          link: "https://docs.google.com/document/d/1lKqrQYRtmsOP9UGHxnAQOOw01lfaMSjDvesVK4l-8wo/edit#heading=h.enryj93vpczc",
         },
         {
-            name: "Backup Soft",
-            links: [
-                {
-                    name: "Google backup",
-                    link: "https://takeout.google.com/settings/takeout"
-                },
-            ],
-            elements: [
-                "[Google] Calendar",
-                "[Google] Chrome (Bookmarks)",
-                "[Google] Drive (Notes)",
-                "Uruchomić automatyzację (alias a3)",
-                "Wypakować backup googla do folderu",
-                "Wyciąć cały folder na zewnętrzny dysk",
-            ]
+          name: "Google Drive",
+          link: "https://drive.google.com/drive/my-drive",
+        },
+      ],
+      elements: [
+        "Główny mail - organizacja",
+        "Prywatny mail - organizacja",
+        "Utworzyć taski z Make Notes",
+        "Docs Quick - organizacja",
+        "Keeps notatki - organizacja",
+        "Google Drive - organizacja",
+      ],
+    },
+    {
+      name: "Backup Soft",
+      links: [
+        {
+          name: "Google backup",
+          link: "https://takeout.google.com/settings/takeout",
+        },
+      ],
+      elements: [
+        "[Google] Calendar",
+        "[Google] Chrome (Bookmarks)",
+        "[Google] Drive (Notes)",
+        "Uruchomić automatyzację (alias a3)",
+        "Wypakować backup googla do folderu",
+        "Wyciąć cały folder na zewnętrzny dysk",
+      ],
+    },
+    {
+      name: "Backup Hard",
+      links: [
+        {
+          name: "Google backup",
+          link: "https://takeout.google.com/settings/takeout",
         },
         {
-            name: "Backup Hard",
-            links: [
-                {
-                    name: "Google backup",
-                    link: "https://takeout.google.com/settings/takeout"
-                },
-            ],
-            elements: [
-                "Przygotować dysk ssd do backupu",
-                "Uruchomić automatyzację (alias a4)",
-                "Wyciąć folder na dysk",
-                "Skopiować cały Evernote notatnik do pliku doc",
-                "Folder Documents (Ubuntu)",
-                "Folder Dane (Dysk D)",
-                "[Google] Calendar",
-                "[Google] Chrome (all)",
-                "[Google] Contacts",
-                "[Google] Drive",
-                "[Google] Keep",
-                "[Google] Mail",
-                "Wypakować backup google do folderu",
-            ]
+          name: "Bitwarden sejf eksport",
+          link: "https://vault.bitwarden.com/#/tools/export",
+        },
+      ],
+      elements: [
+        "Przygotować dysk ssd do backupu",
+        "Uruchomić automatyzację (alias a4)",
+        "Wyciąć folder na dysk",
+        "Skopiować cały Evernote notatnik do pliku doc",
+        "Wyeksportować plik z hasłami (link 2)",
+        "Folder Documents (Ubuntu)",
+        "Folder Dane (Dysk D)",
+        "[Google] Calendar",
+        "[Google] Chrome (all)",
+        "[Google] Contacts",
+        "[Google] Drive",
+        "[Google] Keep",
+        "[Google] Mail",
+        "Wypakować backup google do folderu",
+      ],
+    },
+    {
+      name: "Nowy miesiąc: Biznes",
+      links: [
+        {
+          name: "Prąd",
+          link: "https://logowanie.tauron.pl/",
         },
         {
-            name: "Nowy miesiąc: Biznes",
-            links: [
-                {
-                    name: "Prąd",
-                    link: "https://logowanie.tauron.pl/"
-                },
-                {
-                    name: "PKO",
-                    link: "https://www.ipko.pl//"
-                },
-                {
-                    name: "mBank",
-                    link: "https://online.mbank.pl/pl/Login"
-                },
-                {
-                    name: "Sheet",
-                    link: "https://docs.google.com/spreadsheets/d/1RS4WCPEqILRgId3JlfXZuRd_f6kWZtYg1hzfhM8Y4n8/edit#gid=963549029"
-                },
-            ],
-            elements: [
-                "Zapisać wszystkie dochody",
-                "Aktualizacja arkusza z cyklicznymi płatnościami",
-                "Opłacić wydatki stałe (media, abonamenty, kredyty). Pobrać faktury i potwierdzenia i zapisać kwoty",
-                "Uzupełnić wydatki zmienne z apki",
-                "Przelać kwotę z arkusza W&S na osobne konto (odjąć wcześniej kwotę z apki z kategorii Pomoc",
-                "Zaktualizować kwoty w arkuszu Oszczędności po opłaceniu wszystkiego",
-                "Zapisać zsumowaną kwotę na start kolejnego miesiąca",
-                "Opłacić czynsz za nowy miesiąc i wpisać kwotę",
-                "Przeanalizować ostatni miesiąc",
-            ]
+          name: "PKO",
+          link: "https://www.ipko.pl//",
         },
         {
-            name: "Nowy miesiąc: TODO",
-            links: [
-                {
-                    name: "Sheets",
-                    link: "https://docs.google.com/spreadsheets/d/1RS4WCPEqILRgId3JlfXZuRd_f6kWZtYg1hzfhM8Y4n8/edit#gid=82903271/"
-                },
-            ],
-            elements: [
-                "Skopiować dwie pierwsze kolumny",
-                "Zmienić nazwę miesiąca",
-                "Zmienić kolor drugiej kolumny na czarny",
-                "Insert daty i ustawienie na 1 dzień",
-                "Pierwszy dzień zaznaczyć oraz: pogrubić | zastosować skrót do daty | Format -> Rotation -> Rotate up",
-                "Przeciągnąć dni do końca miesiąca",
-                "Przeciągnąć wzór do końca miesiąca",
-                "Skopiować nie zaznaczony checkbox z któregoś pola",
-                "Zaznaczyć z ctrl wszystkie puste pola",
-                "Wkleić wcześniej zaznaczonego checkboxa",
-                "Uruchomić automatyzację do pythona (alias a1)",
-                "Wstawić formatowanie dat i checkboxów (kolory: green, cornflower blue, red, 2x green, red)",
-                "Ukryć wiersze z czynności za poprzedni miesiąc",
-                "Ustawić kolor kolumny ze wzorem na biały",
-            ]
+          name: "mBank",
+          link: "https://online.mbank.pl/pl/Login",
         },
         {
-            name: "Nowy miesiąc: Trening",
-            links: [
-                {
-                    name: "Sheets",
-                    link: "https://docs.google.com/spreadsheets/d/1RS4WCPEqILRgId3JlfXZuRd_f6kWZtYg1hzfhM8Y4n8/edit#gid=568847193/"
-                },
-            ],
-            elements: [
-                "Wpisać nowy miesiąc i pogrubić",
-                "Insert daty i ustawienie na 1 dzień miesiąca",
-                "Sformatować datę używając pędzla i daty poprzedniej",
-                "Wstawić obok pusty checkbox",
-                "Przeciągnąć datę i checkbox do końca miesiąca",
-                "Uruchomić automatyzację do pythona (alias a2)",
-                "Wstawić formułę do dni i przeciągnąć do końca miesiąca",
-                "Wstawić formuły zliczające",
-                "Szybka analiza miesiąca",
-                "Ukryć rekordy z poprzedniego miesiąca",
-            ]
+          name: "Sheet",
+          link: "https://docs.google.com/spreadsheets/d/1RS4WCPEqILRgId3JlfXZuRd_f6kWZtYg1hzfhM8Y4n8/edit#gid=963549029",
+        },
+      ],
+      elements: [
+        "Zapisać wszystkie dochody",
+        "Aktualizacja arkusza z cyklicznymi płatnościami",
+        "Opłacić wydatki stałe (media, abonamenty, kredyty). Pobrać faktury i potwierdzenia i zapisać kwoty",
+        "Uzupełnić wydatki zmienne z apki",
+        "Przelać kwotę z arkusza W&S na osobne konto (odjąć wcześniej kwotę z apki z kategorii Pomoc",
+        "Zaktualizować kwoty w arkuszu Oszczędności po opłaceniu wszystkiego",
+        "Zapisać zsumowaną kwotę na start kolejnego miesiąca",
+        "Opłacić czynsz za nowy miesiąc i wpisać kwotę",
+        "Przeanalizować ostatni miesiąc",
+      ],
+    },
+    {
+      name: "Nowy miesiąc: TODO",
+      links: [
+        {
+          name: "Sheets",
+          link: "https://docs.google.com/spreadsheets/d/1RS4WCPEqILRgId3JlfXZuRd_f6kWZtYg1hzfhM8Y4n8/edit#gid=82903271/",
+        },
+      ],
+      elements: [
+        "Skopiować dwie pierwsze kolumny",
+        "Zmienić nazwę miesiąca",
+        "Zmienić kolor drugiej kolumny na czarny",
+        "Insert daty i ustawienie na 1 dzień",
+        "Pierwszy dzień zaznaczyć oraz: pogrubić | zastosować skrót do daty | Format -> Rotation -> Rotate up",
+        "Przeciągnąć dni do końca miesiąca",
+        "Przeciągnąć wzór do końca miesiąca",
+        "Skopiować nie zaznaczony checkbox z któregoś pola",
+        "Zaznaczyć z ctrl wszystkie puste pola",
+        "Wkleić wcześniej zaznaczonego checkboxa",
+        "Uruchomić automatyzację do pythona (alias a1)",
+        "Wstawić formatowanie dat i checkboxów (kolory: green, cornflower blue, red, 2x green, red)",
+        "Ukryć wiersze z czynności za poprzedni miesiąc",
+        "Ustawić kolor kolumny ze wzorem na biały",
+      ],
+    },
+    {
+      name: "Nowy miesiąc: Trening",
+      links: [
+        {
+          name: "Sheets",
+          link: "https://docs.google.com/spreadsheets/d/1RS4WCPEqILRgId3JlfXZuRd_f6kWZtYg1hzfhM8Y4n8/edit#gid=568847193/",
+        },
+      ],
+      elements: [
+        "Wpisać nowy miesiąc i pogrubić",
+        "Insert daty i ustawienie na 1 dzień miesiąca",
+        "Sformatować datę używając pędzla i daty poprzedniej",
+        "Wstawić obok pusty checkbox",
+        "Przeciągnąć datę i checkbox do końca miesiąca",
+        "Uruchomić automatyzację do pythona (alias a2)",
+        "Wstawić formułę do dni i przeciągnąć do końca miesiąca",
+        "Wstawić formuły zliczające",
+        "Szybka analiza miesiąca",
+        "Ukryć rekordy z poprzedniego miesiąca",
+      ],
+    },
+    {
+      name: "Comiesięczne czynności",
+      links: [
+        {
+          name: "Sheets Szekle",
+          link: "https://docs.google.com/spreadsheets/d/1u4sHO-mkux9d0xELt6XRo3IVpdoQrDb0cvN-U4ZQZ-E/edit#gid=931574800",
         },
         {
-            name: "Comiesięczne czynności",
-            links: [
-                {
-                    name: "Sheets Szekle",
-                    link: "https://docs.google.com/spreadsheets/d/1u4sHO-mkux9d0xELt6XRo3IVpdoQrDb0cvN-U4ZQZ-E/edit#gid=931574800"
-                },
-                {
-                    name: "Zapasy",
-                    link: "https://keep.google.com/u/0/#LIST/1znx8gChS7FADNMPx1f41echWRGtwrI9Z_w4qoZHy-QYVOdWp-3cldAn2gGERcw"
-                },
-                {
-                    name: "Sprzedaż",
-                    link: "https://docs.google.com/document/d/1lKqrQYRtmsOP9UGHxnAQOOw01lfaMSjDvesVK4l-8wo/edit#bookmark=id.5k59wwenmz3y"
-                },
-                {
-                    name: "Docs",
-                    link: "https://docs.google.com/document/d/1lKqrQYRtmsOP9UGHxnAQOOw01lfaMSjDvesVK4l-8wo/edit#heading=h.b36bz1pk3gth"
-                },
-            ],
-            elements: [
-                "Przebadać j",
-                "Przebadać p (pobieżnie czy nic nie boli, nie krwawi + te do monitorowania porównać)",
-                "Przetestować czujki",
-                "[w sezonie] Sprawdzić luzy i stan śrub w hulajnodze, dopompować dętki",
-                "Zaaplikować sól do zmywarki (1 kg/do pełna)",
-                "[w sezonie] Odkurzyć laser w odświeżaczu i wymienić bawełnianą owijkę",
-                "Kupić potrzebne rzeczy z arkusza ze sklepów online albo zrobić taski do kupna lokalnie. Sprawdzić czy nie ma fajnych ofert na nowe/używane rzeczy na olx/lokalnie (link 1)",
-                "Kupić suplementy/kosmetyki/inne rzeczy, które się skończyły (link 2)",
-                "Sprzedaż - wystawić nowe rzeczy, zaktualizować ceny jak długo się coś nie sprzedaje (link 3)",
-                "Organizacja Temp Docs (link 4)",
-                "Zapisać zdarzenia do arkusza % i M jeśli czegoś nie zapisałem",
-                "Zaktualizować listę (todoist i evernote)",
-                "Pamiętnik uzupełnić",
-            ]
+          name: "Zapasy",
+          link: "https://keep.google.com/u/0/#LIST/1znx8gChS7FADNMPx1f41echWRGtwrI9Z_w4qoZHy-QYVOdWp-3cldAn2gGERcw",
         },
-    ]
+        {
+          name: "Sprzedaż",
+          link: "https://docs.google.com/document/d/1lKqrQYRtmsOP9UGHxnAQOOw01lfaMSjDvesVK4l-8wo/edit#bookmark=id.5k59wwenmz3y",
+        },
+        {
+          name: "Docs",
+          link: "https://docs.google.com/document/d/1lKqrQYRtmsOP9UGHxnAQOOw01lfaMSjDvesVK4l-8wo/edit#heading=h.b36bz1pk3gth",
+        },
+      ],
+      elements: [
+        "Przebadać j",
+        "Przebadać p (pobieżnie czy nic nie boli, nie krwawi + te do monitorowania porównać)",
+        "Przetestować czujki",
+        "[w sezonie] Sprawdzić luzy i stan śrub w hulajnodze, dopompować dętki",
+        "Zaaplikować sól do zmywarki (1 kg/do pełna)",
+        "[w sezonie] Odkurzyć laser w odświeżaczu i wymienić bawełnianą owijkę",
+        "Kupić potrzebne rzeczy z arkusza ze sklepów online albo zrobić taski do kupna lokalnie. Sprawdzić czy nie ma fajnych ofert na nowe/używane rzeczy na olx/lokalnie (link 1)",
+        "Kupić suplementy/kosmetyki/inne rzeczy, które się skończyły (link 2)",
+        "Sprzedaż - wystawić nowe rzeczy, zaktualizować ceny jak długo się coś nie sprzedaje (link 3)",
+        "Organizacja Temp Docs (link 4)",
+        "Zapisać zdarzenia do arkusza M jeśli czegoś nie zapisałem",
+        "Zaktualizować listę (todoist i evernote)",
+        "Pamiętnik uzupełnić",
+      ],
+    },
+  ];
 }
 
 export default Data;
